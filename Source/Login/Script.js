dd6c3e87e15fd6daf767e2b56d07aa2a36b9f661
Maite <maitefranca08@gmail.com>
2026-06-18 11:10:37 -0300

    Add files via upload

---
+++ Source/Login/Script.js
@@ -0,0 +1,10 @@
+function login(){
+    var nome = $("#nome").val()
+    var senha = $("#senha").val()
+
+    if(nome && senha & nome ==="admin" & senha === "12345"){
+
+    }else{
+        
+    }
+}
